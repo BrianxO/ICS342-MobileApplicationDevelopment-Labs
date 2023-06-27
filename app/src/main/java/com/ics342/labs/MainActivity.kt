389198df7221d1678a549ac4de5b0140188933fe
brian <brianombiro@gmail.com>
2023-06-26 20:20:46 -0500

    Lab 4

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,13 +3,18 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,7 +57,7 @@ class MainActivity : ComponentActivity() {
             LabsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    DataItemList(dataItems = dataItems)
                 }
             }
         }
@@ -67,23 +72,39 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-
 @Composable
 fun DataItemView(dataItem: DataItem) {
-    /* Create the view for the data item her. */
-    Text(
-        text = "ID: ${dataItem.id}",
-        modifier = Modifier.padding(start = 23.dp),
+    val (showItemDialog, setShowItemDialog) = remember { mutableStateOf(false) }
 
-    )
-    Column(modifier = Modifier.padding(start = 72.dp)) {
+    Column(modifier = Modifier.clickable { setShowItemDialog(true) }) {
         Text(
-            text = dataItem.name,
-            modifier = Modifier.padding(top = 14.dp)
+            text = "ID: ${dataItem.id}",
+            modifier = Modifier.padding(start = 23.dp)
         )
-        Text(
-            text = dataItem.description,
-            modifier = Modifier.padding(top = 12.dp)
+        Column(modifier = Modifier.padding(start = 72.dp)) {
+            Text(
+                text = dataItem.name,
+                modifier = Modifier.padding(top = 14.dp)
+            )
+            Text(
+                text = dataItem.description,
+                modifier = Modifier.padding(top = 12.dp)
+            )
+        }
+    }
+
+    if (showItemDialog) {
+        AlertDialog(
+            onDismissRequest = { setShowItemDialog(false) },
+            title = { Text(dataItem.name) },
+            text = { Text(dataItem.description) },
+            confirmButton = {
+                Button(
+                    onClick = { setShowItemDialog(false) }
+                ) {
+                    Text("Okay")
+                }
+            }
         )
     }
 }
